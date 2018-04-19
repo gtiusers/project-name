000e84417581e421094f2809eb39f7b1e81e2dd3
gitusers <1578972407@qq.com>
2018-04-19 11:14:25 +0800

    first commit file

---
+++ test_Math.py
@@ -0,0 +1,21 @@
+from caculator import Math
+import unittest
+
+class TestMath(unittest.TestCase):
+    def setUp(self):
+        print("start test")
+
+    def test_add(self):
+        j=Math(5,10)
+        self.assertEqual(j.add(),15)
+        #self.assertEqual(j.add(),12)
+
+    def tearDown(self):
+        print("test end")
+
+if __name__=='__main__':
+    suit=unittest.TestSuite()
+    suit.addTest(TestMath("test_add"))
+
+    runner=unittest.TextTestRunner()
+    runner.run(suit)
